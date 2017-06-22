7cd9337d3f33192b72ee184ad1de976e9cabeac7
Keegan Kelly <keegankelly1@gmail.com>
2017-06-22 07:22:41 -0500

    Added package.json. Set up mongoose connection to local mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+
 
 //Server config
 const app = express();
@@ -7,6 +9,18 @@ const PORT = process.env.PORT || 3000;
 
 //View Engine Setup
 app.set("views", path.join(__dirname, "views"));
+
+//Database config
+mongoose.connect(/*NEED TO ADD REMOTE ENV HERE*/ "mongodb://localhost/My_Local_DB");
+const db = mongoose.connection;
+
+db.on("error", function(err){
+  console.log("There was a Mongoose error: ", err)
+});
+
+db.once("open", function(){
+  console.log("Mongoose connection successful.")
+});
 
 //Serve static content from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
