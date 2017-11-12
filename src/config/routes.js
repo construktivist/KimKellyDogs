53b0bba632b55d9fd3ec431c90b3a176e35e2d72
Keegan Kelly <keegankelly1@gmail.com>
2017-11-12 15:10:11 -0600

    Updated routes to work with react-router v4. Routes are now functional

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,15 +1,19 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Main from "../main/components/Main"
 import Admin from "../admin/components/Admin"
 
+const App = () =>(
+  <Switch>
+    <Route exact path="/" component={Main} />
+    <Route path="/admin" component={Admin} />
+  </Switch>
+)
+
 
 module.exports = (
   <Router>
-    <div>
-      <Route exact path="/" component={Main} />
-      <Route path="/admin" component={Admin} />
-    </div>
+    <App />
   </Router>
 );
